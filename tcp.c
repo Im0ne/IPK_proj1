b5f020cf15928b8feb44fcb27718774edd2dac47
xonufr00 <xonufr00@stud.fit.vutbr.cz>
2024-04-01 15:43:59 +0200

    fix: fixed writing 2 newlines instead of 1

--- tcp.c
+++ tcp.c
@@ -349,7 +349,7 @@ int tcp_connect(char* ipstr, int port) {
 
                     // Check if we've filled the buffer
                     if (total_bytes == sizeof(server_reply)) {
-                        error_tcp("Message is too long\n");
+                        error_tcp("Message is too long");
                         cleanup(socket_desc_tcp, epollfd_tcp);
                         return EXIT_FAILURE;
                     }
